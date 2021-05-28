a125764c18a59b94721ea1894926432c92d30f47
edweenie123 <edwinchen1000@gmail.com>
2021-05-28 12:04:48 -0400

    final try

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "../include/coordinate.h"
+
+using namespace std;
+
+int main() {
+    Coordinate boi(1,2,3);
+    cout<<boi.toString()<<endl;
+}
--- src/rook.cpp
+++ src/rook.cpp
@@ -1,5 +1,10 @@
-#include<../include/rook.h>
+#include "../include/rook.h"
 
-// Rook::Rook(int col_, int row_, int lvl_, int color_){
+Rook::Rook(int row_, int col_, int lvl_, int color_){
+    // location = Coordinate(row_, col_, lvl_);
+    // color = color_;
+};
 
-// };
+vector<Coordinate> Rook::getMoves() {
+
+}
