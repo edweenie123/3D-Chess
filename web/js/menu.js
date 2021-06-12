c09becccb396e512986803d5041e6d3fda1af88d
edweenie123 <edwinchen1000@gmail.com>
2021-06-12 19:02:09 -0400

    fixed wrong color bug and put menu in own file

---
+++ web/js/menu.js
@@ -0,0 +1,60 @@
+class Menu {
+  constructor(board) {
+    this.board = board;
+    this.colour = 1; // Assume player wants to play as white
+    this.gameMode;
+    this.menuDiv = document.getElementById("selec");
+    document.getElementById("playerToggle").onclick = () => {this.toggle()};
+    document.getElementById("submitButton").onclick = () => {this.submit()};
+    this.init();
+  }
+
+  submit() {
+    if (document.getElementById("sel1").checked == true) {
+      this.gameMode = document.getElementById("sel1").value;
+    } else if (document.getElementById("sel2").checked == true) {
+      this.gameMode = document.getElementById("sel2").value;
+    } else if (document.getElementById("sel3").checked == true) {
+      this.gameMode = document.getElementById("sel3").value;
+    } else if (document.getElementById("sel4").checked == true) {
+      this.gameMode = document.getElementById("sel4").value;
+    }
+
+    console.log(this.gameMode);
+    console.log(this.colour);
+
+
+    board.cpuDifficulty = this.gameMode;
+    if (this.colour === -1) this.board.compActive = true;
+    board.opponent = new Module.Solver(Number(board.cpuDifficulty));
+
+    this.menuDiv.style.display = "none";
+    document.getElementById("board").style.display = "block";
+
+    board.changeTurn();
+    console.log(board.turn);
+    console.log(board.cpuDifficulty);
+  }
+
+  toggle() {
+    this.colour = this.colour === 1 ? -1 : 1;
+    this.init();
+  }
+
+  init() {
+    document.getElementById("chosenColour").innerHTML =
+      (this.colour === 1) ? "WHITE" : "BLACK";
+  }
+}
+
+// var gameMode = "NULL";
+// var colour = 1;
+
+// if (document.getElementById("sel5").checked == true) {
+//   colour = -1;
+// }
+
+// var chosenColour = "WHITE&nbsp;&nbsp;&nbsp;&nbsp;";
+// if (colour == -1) {
+//   chosenColour = "BLACK&nbsp;&nbsp;&nbsp;&nbsp;";
+// }
